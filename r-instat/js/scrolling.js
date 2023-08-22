d6380826a3de02d400796aa77a506a2d072b6681
Ntalumeso <antoine.ntalumeso@gmail.com>
2023-08-22 21:01:21 +0300

    Delete r-instat directory

--- r-instat/js/scrolling.js
+++
@@ -1,9 +0,0 @@
-const scrollToTopButton = document.querySelector(".scroll-to-top-btn");
-
-window.addEventListener("scroll", () => {
-  if(this.window.pageYOffset > 100){
-    scrollToTopButton.classList.add("active");
-  }else{
-      scrollToTopButton.classList.remove("active");
-    }
-});
